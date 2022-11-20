0d132b56b274c1b5c4ba31ad3986e68a5028e1a4
Vicky <esqui1949@gmail.com>
2022-11-20 09:06:33 +0100

    Proyecto Futuro

---
+++ src/proyectofuturonetbeans/ProyectoFuturoNetBeans.java
@@ -0,0 +1,26 @@
+
+package proyectofuturonetbeans;
+
+
+public class ProyectoFuturoNetBeans {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Estudiante: "+args[0]);
+        String valor1=args[1];
+        String valor2 = args[2];
+        System.out.println(veredicto(valor2,valor1 )); 
+    }
+    
+     static String veredicto(String valor1, String valor2){
+
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+    
+}
